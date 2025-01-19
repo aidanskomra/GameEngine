ba7d824c404362b1dbc07a828757aaf864b0d74e
Aidan Skomra <skomraa@sheridancollege.ca>
2025-01-19 14:07:14 -0500

    Completed

--- GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include "NoCommandDemo.h"
 
 int main()
 {
-
+	NoCommandDemo* ncd = new NoCommandDemo();
+	std::cout << "No Commands:\n";
+	ncd->HandleInput("X");
+	ncd->HandleInput("Y");
+	ncd->HandleInput("A");
+	ncd->HandleInput("B");
+	delete ncd;
 }
---
+++ GameEngine/NoCommandDemo.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+class NoCommandDemo
+{
+public:
+	void HandleInput(std::string _button);
+
+private:
+	void Jump();
+	void FireGun();
+	void SwapWeapon();
+	void Lurch();
+};
+
