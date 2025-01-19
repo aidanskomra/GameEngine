59d4ed91ae2214841a49c905a3d9f98d68b8d3a2
Aidan Skomra <skomraa@sheridancollege.ca>
2025-01-19 17:55:02 -0500

    Completed!

--- GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine.cpp
@@ -4,6 +4,6 @@
 int main()
 {
 	ThreadTest* ttest = new ThreadTest();
-	ttest->TestFunctionObject();
+	ttest->TestLambdaFunction();
 	delete ttest;
 }
--- GameEngine/ThreadTest.cpp
+++ GameEngine/ThreadTest.cpp
@@ -2,21 +2,19 @@
 #include <iostream>
 #include <chrono>
 
-void ThreadTest::TestFunctionObject() {
-	m_thread = std::thread(ThreadTest());
+void ThreadTest::TestLambdaFunction() {
+	m_thread = std::thread([]
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				std::cout << "thread Lambda function Executing" << std::endl;
+				std::this_thread::sleep_for(std::chrono::milliseconds(200));
+			}
+		});
 	for (int i = 0; i < 5; i++)
 	{
 		std::cout << "Display From MainThread" << std::endl;
 		std::this_thread::sleep_for(std::chrono::milliseconds(200));
 	}
 	m_thread.join();
-}
-
-void ThreadTest::operator()()
-{
-	for (int i = 0; i < 5; i++)
-	{
-		std::cout << "thread function object Executing" << std::endl;
-		std::this_thread::sleep_for(std::chrono::milliseconds(200));
-	}
 }
